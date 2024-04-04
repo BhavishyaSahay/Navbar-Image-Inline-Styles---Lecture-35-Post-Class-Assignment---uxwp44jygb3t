4a111243bc09da06de55742444cba350456a824c
Bhavishya Sahay <sahaybhavishya@gmail.com>
2024-04-04 22:29:16 +0530

    Added_inline_css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import "./App.css";
 import pic from "./assets/NewtonSchool.jpg";
 
 const App = () => {
+  var style={
+    width: '130px',
+    height: '100px',
+    margin: '10px',
+  }
   return (
     <div id="navbar">
       <div>NavBar</div>
-      <img />
+      <img src={pic} style={style}/>
     </div>
   );
 };
